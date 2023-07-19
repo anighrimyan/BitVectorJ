ee81077973f495b058c79dbb2dd82125fe9e12ba
Ani Ghrimyan <89627723+anighrimyan@users.noreply.github.com>
2023-07-19 15:39:12 +0400

    Update BitVector.java

--- BitVector.java
+++ BitVector.java
@@ -1,104 +1,177 @@
 import java.util.ArrayList;
+
+/**
+ * The BitVector class represents a bit vector that allows setting and resetting bits at specified positions.
+ * It uses an ArrayList to store the bits, and each element in the ArrayList corresponds to a 64-bit block.
+ * The bits are addressed by position, and the position is zero-based.
+ */
+
 public class BitVector {
     private int arraySize;
     private int position;
     private int elementIndex;
     private int bitPosition;
-    private final int bitStep = 64;
-    int size = bitVector.size();
-    private final static ArrayList<Integer> bitVector;
+    private final int BIT_STEP = 64;
+    private int size = bitVector.size();
+    private final static ArrayList<Integer> BIT_VECTOR;
     static
     {
-        bitVector = new ArrayList<>();
+       BIT_VECTOR = new ArrayList<>();
     }
+
+    /**
+     * Checks if the size of the bitVector ArrayList is greater than or equal to the required array size.
+     * @return true if the size is greater than or equal to the required array size, false otherwise.
+     */
+    
     private boolean checkArraySize() {
-        return bitVector.size() >= arraySize;
+        return BIT_VECTOR.size() >= arraySize;
     }
+
+    /**
+     * Calculates the required array size based on the current position and sets the elementIndex and arraySize fields accordingly.
+     */
+    
     private void countArraySize() {
-        if (position % bitStep == 0) {
-            this.elementIndex = this.arraySize = position / bitStep;
+        if (position % BIT_STEP == 0) {
+            this.elementIndex = this.arraySize = position / BIT_STEP;
         } else {
-            this.elementIndex = this.arraySize = position / bitStep + 1;
+            this.elementIndex = this.arraySize = position / BIT_STEP + 1;
         }
     }
+
+    
+    /**
+     * Adds zero elements to the beginning of the bitVector ArrayList to match the required array size.
+     */
+    
     private void setBitVectorElementInFront() {
         int temp = arraySize;
         while (temp - size > 0) {
-            bitVector.add(0,0);
+            BIT_VECTOR.add(0,0);
             --temp;
         }
     }
+
+    /**
+     * Adds zero elements to the end of the bitVector ArrayList to match the required array size.
+     */
+    
     private void setBitVectorElement() {
         int temp = arraySize;
         while (temp > 0) {
-            bitVector.add(0);
+            BIT_VECTOR.add(0);
             --temp;
         }
     }
+
+    /**
+     * Sets the position value.
+     * @param pos The position value to set.
+     * @throws ArrayIndexOutOfBoundsException if the given position is negative.
+     */
+    
     private void setPositionValue(int pos) {
         if (pos >= 0)
           this.position = pos;
         System.out.println("Invalid input.");
         System.exit(-1);
     }
+
+    /**
+     * Checks if the given position value is valid (non-negative).
+     * @param pos The position value to check.
+     * @return true if the position value is valid (non-negative), false otherwise.
+     */
+    
     private boolean checkPositionValue(int pos) {
         return pos >= 0;
     }
+
+    /**
+     * Prints the content of the bitVector ArrayList before setting a bit.
+     */
+    
     private void printBitVectorBeforeSet() {
-        System.out.println("The bitVector before setting: ");
-        for (int el : bitVector) {
+        System.out.println("The BIT_VECTOR before setting: ");
+        for (int el : BIT_VECTOR) {
             System.out.print(Integer.toString(el, 2));
         }
     }
+
+    /**
+     * Prints the content of the bitVector ArrayList after setting a bit.
+     */
+    
     private void printBitVectorAfterSet() {
-        System.out.println("The bitVector After setting: ");
-        for (int el : bitVector) {
+        System.out.println("The BIT_VECTOR After setting: ");
+        for (int el : BIT_VECTOR) {
             System.out.print(Integer.toString(el, 2) + " ");
         }
     }
+
+    
+    /**
+     * Finds the corresponding bit position in the element based on the current position.
+     */
+    
     private void findBitPositionInElement() {
-        if (position > bitStep) {
+        if (position > BIT_STEP) {
             bitPosition = position;
-            bitPosition -= bitStep;
-                while (!(bitPosition < bitStep)) {
-                    bitPosition -= bitStep;
+            bitPosition -= BIT_STEP;
+                while (!(bitPosition < BIT_STEP)) {
+                    bitPosition -= BIT_STEP;
                 }
         } else {
-            bitPosition = bitStep - position;
+            bitPosition = BIT_STEP - position;
         }
     }
+
+     /**
+     * Sets the bit at the given position to 1.
+     * @param pos The position of the bit to be set.
+     * @throws ArrayIndexOutOfBoundsException if the position is negative or greater than the allowed maximum.
+     */
+    
     public void set(int pos) {
-        if (bitVector.isEmpty()) {
+        if (BIT_VECTOR.isEmpty()) {
             setBitVectorElement();
         }
-        if (!bitVector.isEmpty()) {
+        if (!BIT_VECTOR.isEmpty()) {
             if (checkPositionValue(pos)) {
                 setPositionValue(pos);
                 countArraySize();
                 if (checkArraySize()) {
                     findBitPositionInElement();
-                    bitVector.set(elementIndex, bitVector.get(elementIndex) | (1 << bitPosition));
+                    BIT_VECTOR.set(elementIndex, BIT_VECTOR.get(elementIndex) | (1 << bitPosition));
                 } else {
                     setBitVectorElementInFront();
                     findBitPositionInElement();
-                    bitVector.set(elementIndex, bitVector.get(elementIndex) | (1 << bitPosition));
+                    BIT_VECTOR.set(elementIndex, BIT_VECTOR.get(elementIndex) | (1 << bitPosition));
                 }
             } else {
                 throw new ArrayIndexOutOfBoundsException();
             }
         }
     }
+
+    /**
+     * Resets the bit at the given position to 0.
+     * @param pos The position of the bit to be reset.
+     * @throws ArrayIndexOutOfBoundsException if the position is negative or greater than the allowed maximum.
+     */
+    
     public void reset(int pos) {
-        if (bitVector.isEmpty()) {
-           System.out.println("bitVector is empty.");
+        if (BIT_VECTOR.isEmpty()) {
+           System.out.println("BIT_VECTOR is empty.");
            System.exit(1);
         }
-        if (!bitVector.isEmpty()) {
+        if (!BIT_VECTOR.isEmpty()) {
             if (checkPositionValue(pos)) {
                 countArraySize();
                 if (checkArraySize()) {
                     findBitPositionInElement();
-                    bitVector.set(elementIndex, bitVector.get(elementIndex) & (~(1 << bitPosition)));
+                    BIT_VECTOR.set(elementIndex, BIT_VECTOR.get(elementIndex) & (~(1 << bitPosition)));
                 } else {
                     throw new ArrayIndexOutOfBoundsException();
                 }
